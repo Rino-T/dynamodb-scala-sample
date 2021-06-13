ccfd223b033dd8c703708b27f5989b5dbe8d2177
千葉俊典 <toshinori.chiba.p@gmail.com>
2021-06-13 14:40:25 +0900

    delete table

---
+++ src/main/scala/DeleteTable.scala
@@ -0,0 +1,36 @@
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.{
+  DeleteTableRequest,
+  DeleteTableResponse,
+  DynamoDbException,
+  ResourceNotFoundException
+}
+
+import java.net.URI
+import scala.util.{Failure, Success, Try}
+
+object DeleteTable {
+
+  def main(args: Array[String]): Unit = {
+    val client: DynamoDbClient = DynamoDbClient.builder().endpointOverride(URI.create("http://localhost:8000")).build()
+    deleteDynamoDbTable(client, "roles")
+  }
+
+  def deleteDynamoDbTable(ddb: DynamoDbClient, tableName: String): Option[DeleteTableResponse] = {
+    val request = DeleteTableRequest.builder().tableName(tableName).build()
+
+    Try {
+      ddb.deleteTable(request)
+    } match {
+      case Failure(exception) =>
+        exception match {
+          case e: ResourceNotFoundException =>
+            println(s"table $tableName is not found")
+            None
+          case e: DynamoDbException => throw e
+        }
+      case Success(response) => Some(response)
+    }
+  }
+
+}
